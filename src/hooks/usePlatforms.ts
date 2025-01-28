f52e4f8690d241603cbf6e9b54ff34012c2a77a7
joeri <jriz0002@student.monash.edu>
2025-01-29 01:05:07 +1100

    refactor - create local hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -13,7 +13,7 @@ export interface Platform {
 const usePlatforms = () =>  useQuery<FetchResponse<Platform>, Error>({
   queryKey: ["platforms"],
   queryFn: () => apiClient.getAll,
-  initialData: {count: platforms.length , results: platforms},
+  initialData: {count: platforms.length , results: platforms, next: null},
   staleTime: 24*60*60*1000
 });
 
