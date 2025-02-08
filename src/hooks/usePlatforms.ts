9d0cc09c26af7fce395c3604e661e3d075c51650
joeri <jriz0002@student.monash.edu>
2025-02-09 01:08:24 +1100

    Refactor - Moved entity interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,14 +1,9 @@
 import { QueryKey, useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient, { FetchResponse } from "../services/api-client";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents")
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatforms = () =>  useQuery<FetchResponse<Platform>, Error>({
   queryKey: ["platforms"],
