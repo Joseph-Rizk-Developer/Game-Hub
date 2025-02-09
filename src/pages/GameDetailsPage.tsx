7480f970ed9e64dbfca9786c59ca9f4d29975c36
joeri <jriz0002@student.monash.edu>
2025-02-09 14:37:42 +1100

    Build Game Attributes

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import CriticScore from "../components/CriticScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailsPage = () => {
   const { id } = useParams();
-  console.log(id);
-
   const { data: game, isLoading, error } = useGame(id!);
 
   if (isLoading) return <Spinner />;
@@ -17,6 +18,7 @@ const GameDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
